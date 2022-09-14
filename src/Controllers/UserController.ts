a1c3c46743a45f18ad5ae85431cbba45aec91adb
Oreki-Dev <karan.ajwani2404@gmail.com>
2022-09-14 17:37:19 +0530

    update UserController.ts

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../Utils/Queries";
 import { AniFetch } from "../Utils/Anilist";
+import { ReplaceHtml } from "../Utils/Functions";
 
 export const user = async (req: Request, res: Response) => {
     const id: string = req.params.id as string;
@@ -9,6 +10,7 @@ export const user = async (req: Request, res: Response) => {
     };
     const { data } = await AniFetch(User, variables);
     res.render("user", {
+      replaceHtml: ReplaceHtml,
       user: data.User,
     });
   };
