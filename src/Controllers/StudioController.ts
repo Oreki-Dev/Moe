e5ec4dc443763fc736296916f4bdab83ec98d956
Oreki-Dev <Oreki-Dev@users.noreply.github.com>
2022-09-14 11:41:58 +0000

    Prettified Code!

--- src/Controllers/StudioController.ts
+++ src/Controllers/StudioController.ts
@@ -3,12 +3,12 @@ import { Studio } from "../Utils/Queries";
 import { AniFetch } from "../Utils/Anilist";
 
 export const studio = async (req: Request, res: Response) => {
-    const id: string = req.params.id as string;
-    const variables = {
-      id: parseInt(id),
-    };
-    const { data } = await AniFetch(Studio, variables);
-    res.render("studio", {
-      studio: data.Studio,
-    });
+  const id: string = req.params.id as string;
+  const variables = {
+    id: parseInt(id),
   };
+  const { data } = await AniFetch(Studio, variables);
+  res.render("studio", {
+    studio: data.Studio,
+  });
+};
