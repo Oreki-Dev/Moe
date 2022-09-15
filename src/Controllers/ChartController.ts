302daa3f5d342d1eb78e4cb1f738ad338aa50ee3
Oreki-Dev <Oreki-Dev@users.noreply.github.com>
2022-09-15 06:46:34 +0000

    Prettified Code!

--- src/Controllers/ChartController.ts
+++ src/Controllers/ChartController.ts
@@ -3,31 +3,31 @@ import { AiringUpcoming } from "../Utils/Queries";
 import { AniFetch, Season, NextSeason } from "../Utils/Anilist";
 
 export const chart = async (req: Request, res: Response) => {
-    const season: string = req.query.season as string;
-    if (!season) return res.redirect("/chart?season=airing")
-    if (season.toLowerCase() === "airing") {
-        const variables = {
-            page: 1,
-            seasonYear: Season().seasonYear,
-            season: Season().season,
-            sort: "POPULARITY_DESC",
-        };
-        const { data } = await AniFetch(AiringUpcoming, variables);
-        res.render("chart", {
-          animes: data.Page.media,
-        });
-    } else if (season.toLowerCase() === "upcoming") {
-        const variables = {
-            page: 1,
-            seasonYear: NextSeason().seasonYear,
-            season: NextSeason().season,
-            sort: "POPULARITY_DESC",
-          };
-          const { data } = await AniFetch(AiringUpcoming, variables);
-          await res.render("chart", {
-            animes: data.Page.media,
-          });
-    } else {
-        return res.redirect("/chart?season=airing")
-    }
-  };
+  const season: string = req.query.season as string;
+  if (!season) return res.redirect("/chart?season=airing");
+  if (season.toLowerCase() === "airing") {
+    const variables = {
+      page: 1,
+      seasonYear: Season().seasonYear,
+      season: Season().season,
+      sort: "POPULARITY_DESC",
+    };
+    const { data } = await AniFetch(AiringUpcoming, variables);
+    res.render("chart", {
+      animes: data.Page.media,
+    });
+  } else if (season.toLowerCase() === "upcoming") {
+    const variables = {
+      page: 1,
+      seasonYear: NextSeason().seasonYear,
+      season: NextSeason().season,
+      sort: "POPULARITY_DESC",
+    };
+    const { data } = await AniFetch(AiringUpcoming, variables);
+    await res.render("chart", {
+      animes: data.Page.media,
+    });
+  } else {
+    return res.redirect("/chart?season=airing");
+  }
+};
