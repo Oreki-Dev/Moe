948ec168657d23e57373e0a9ace74b926f182139
Oreki-Dev <Oreki-Dev@users.noreply.github.com>
2022-09-16 06:42:37 +0000

    Prettified Code!

--- src/Controllers/ChartController.ts
+++ src/Controllers/ChartController.ts
@@ -4,46 +4,46 @@ import { AniFetch, Season, NextSeason } from "../Utils/Anilist";
 import moment from "moment";
 
 export const chart = async (req: Request, res: Response) => {
-    const season: string = req.query.season as string;
-    if (!season) return res.redirect("/chart?season=airing")
-    if (season.toLowerCase() === "airing") {
-        const variables = {
-            page: 1,
-            seasonYear: Season().seasonYear,
-            season: Season().season,
-            sort: "POPULARITY_DESC",
-        };
-        const { data } = await AniFetch(AiringUpcoming, variables);
-        res.render("chart", {
-          airing: true,
-          animes: data.Page.media,
-        });
-    } else if (season.toLowerCase() === "upcoming") {
-        const variables = {
-            page: 1,
-            seasonYear: NextSeason().seasonYear,
-            season: NextSeason().season,
-            sort: "POPULARITY_DESC",
-          };
-          const { data } = await AniFetch(AiringUpcoming, variables);
-          await res.render("chart", {
-            airing: false,
-            animes: data.Page.media,
-          });
-    } else {
-        return res.redirect("/chart?season=airing")
-    }
-  };
-
-  export const schedule = async (req: Request, res: Response) => {
+  const season: string = req.query.season as string;
+  if (!season) return res.redirect("/chart?season=airing");
+  if (season.toLowerCase() === "airing") {
     const variables = {
       page: 1,
-      start: moment().startOf("day").unix(),
-      end: moment().endOf("day").unix(),
+      seasonYear: Season().seasonYear,
+      season: Season().season,
+      sort: "POPULARITY_DESC",
     };
-    const { data } = await AniFetch(Schedule, variables);
-    await res.render("schedule", {
-      moment: moment,
-      animes: data.Page.airingSchedules,
-    })
+    const { data } = await AniFetch(AiringUpcoming, variables);
+    res.render("chart", {
+      airing: true,
+      animes: data.Page.media,
+    });
+  } else if (season.toLowerCase() === "upcoming") {
+    const variables = {
+      page: 1,
+      seasonYear: NextSeason().seasonYear,
+      season: NextSeason().season,
+      sort: "POPULARITY_DESC",
+    };
+    const { data } = await AniFetch(AiringUpcoming, variables);
+    await res.render("chart", {
+      airing: false,
+      animes: data.Page.media,
+    });
+  } else {
+    return res.redirect("/chart?season=airing");
   }
+};
+
+export const schedule = async (req: Request, res: Response) => {
+  const variables = {
+    page: 1,
+    start: moment().startOf("day").unix(),
+    end: moment().endOf("day").unix(),
+  };
+  const { data } = await AniFetch(Schedule, variables);
+  await res.render("schedule", {
+    moment: moment,
+    animes: data.Page.airingSchedules,
+  });
+};
--- src/Controllers/GenreController.ts
+++ src/Controllers/GenreController.ts
@@ -3,57 +3,57 @@ import { Genre } from "../Utils/Queries";
 import { AniFetch } from "../Utils/Anilist";
 
 const GenreCollection: string[] = [
-    "action",
-    "adventure",
-    "comedy",
-    "drama",
-    "ecchi",
-    "fantasy",
-    "hentai",
-    "horror",
-    "mahou-shoujo",
-    "mecha",
-    "music",
-    "mystery",
-    "msychological",
-    "romance",
-    "sci-fi",
-    "slice-of-life",
-    "sports",
-    "supernatural",
-    "shriller"
-  ];
+  "action",
+  "adventure",
+  "comedy",
+  "drama",
+  "ecchi",
+  "fantasy",
+  "hentai",
+  "horror",
+  "mahou-shoujo",
+  "mecha",
+  "music",
+  "mystery",
+  "msychological",
+  "romance",
+  "sci-fi",
+  "slice-of-life",
+  "sports",
+  "supernatural",
+  "shriller",
+];
 
 export const genre = async (req: Request, res: Response) => {
-    let genre: string = req.params.name as string
-    if (!GenreCollection.includes(genre.toLowerCase())) return res.render("404")
+  let genre: string = req.params.name as string;
+  if (!GenreCollection.includes(genre.toLowerCase())) return res.render("404");
 
-    let query: string = req.query.page as string
-    if (!query) query = "1"
+  let query: string = req.query.page as string;
+  if (!query) query = "1";
 
-    if (genre.toLowerCase().startsWith("sci")) {
-      genre = genre
-    } else {
-      genre = genre.replace("-", " ").replace("-", " ")
-    }
+  if (genre.toLowerCase().startsWith("sci")) {
+    genre = genre;
+  } else {
+    genre = genre.replace("-", " ").replace("-", " ");
+  }
 
-    const variables = {
-        page: parseInt(query),
-        genre: genre
-    }
+  const variables = {
+    page: parseInt(query),
+    genre: genre,
+  };
 
-    try {
-        const { data } = await AniFetch(Genre, variables);
-        res.render("genre", {
-            error: null,
-            genre: genre,
-            animes: data.Page.media
-        });
-    } catch (error) {
-       res.render("genre", {
-        error: "An Error Happened",
-        genre: genre,
-        animes: null
-       }) 
-    }
+  try {
+    const { data } = await AniFetch(Genre, variables);
+    res.render("genre", {
+      error: null,
+      genre: genre,
+      animes: data.Page.media,
+    });
+  } catch (error) {
+    res.render("genre", {
+      error: "An Error Happened",
+      genre: genre,
+      animes: null,
+    });
+  }
 };
