ae325bcb6a6e7174d7c9e57cdbb450eb76add549
Oreki-Dev <karan.ajwani2404@gmail.com>
2022-09-23 12:35:38 +0530

    update GenreController.ts

--- src/Controllers/GenreController.ts
+++ src/Controllers/GenreController.ts
@@ -2,40 +2,40 @@ import { Request, Response } from "express";
 import { Genre } from "../Utils/Queries";
 import { AniFetch } from "../Utils/Anilist";
 
-const GenreCollection: string[] = [
-  "action",
-  "adventure",
-  "comedy",
-  "drama",
-  "ecchi",
-  "fantasy",
-  "hentai",
-  "horror",
-  "mahou-shoujo",
-  "mecha",
-  "music",
-  "mystery",
-  "psychological",
-  "romance",
-  "sci-fi",
-  "slice-of-life",
-  "sports",
-  "supernatural",
-  "shriller",
-];
+interface StringArray {
+  [index: string]: string;
+}
+
+const GenreCollection: StringArray = {
+  "action": "action",
+  "adventure": "adventure",
+  "comedy": "comedy",
+  "drama": "drama",
+  "ecchi": "ecchi",
+  "fantasy": "fantasy",
+  "hentai": "hentai",
+  "horror": "horror",
+  "mahou-shoujo": "mahou shoujo",
+  "mecha": "mecha",
+  "music": "music",
+  "mystery": "mystery",
+  "psychological": "psychological",
+  "romance": "romance",
+  "sci-fi": "sci-fi",
+  "slice-of-life": "slice of life",
+  "sports": "sports",
+  "supernatural": "supernatural",
+  "thriller": "thriller",
+}
 
 export const genre = async (req: Request, res: Response) => {
   let genre: string = req.params.name as string;
-  if (!GenreCollection.includes(genre.toLowerCase())) return res.render("404");
+  if (!GenreCollection.hasOwnProperty(genre.toLowerCase())) return res.render("404");
 
   let query: string = req.query.page as string;
   if (!query) query = "1";
 
-  if (genre.toLowerCase().startsWith("sci")) {
-    genre = genre;
-  } else {
-    genre = genre.replace("-", " ").replace("-", " ");
-  }
+  genre = GenreCollection[genre.toLowerCase()]
 
   const variables = {
     page: parseInt(query),
