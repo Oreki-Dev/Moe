b1955cc26d061416a13a2ad5756facb0f60ad3b2
Oreki <77574238+Oreki-Dev@users.noreply.github.com>
2022-09-29 09:59:42 +0530

    Update App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import compression from "compression";
 import bodyParser from "body-parser";
-import rateLimit from "express-rate-limit";
 
 import * as HomeController from "./Controllers/HomeController";
 import * as AnimeController from "./Controllers/AnimeController";
@@ -15,22 +14,12 @@ import * as BirthdayController from "./Controllers/BirthdayController";
 
 const App = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-  handler: (req: Request, res: Response) => res.render("rateLimited"),
-});
-
 App.set("view engine", "ejs");
 App.use(compression());
 App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({ extended: true }));
 App.disable("x-powered-by");
 App.use(express.static("public"));
-
-App.use(limiter);
 
 App.get("/", HomeController.index);
 App.get("/anime/:id", AnimeController.anime);
